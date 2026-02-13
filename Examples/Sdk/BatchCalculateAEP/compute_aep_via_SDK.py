7646e9783be6348e5cf78b6d249963bc04cb84ef
Tom Levick <tom.levick@dnv.com>
2026-02-13 14:11:21 +0000

    updated default Windfarmer desktop versions

--- Examples/Sdk/BatchCalculateAEP/compute_aep_via_SDK.py
+++ Examples/Sdk/BatchCalculateAEP/compute_aep_via_SDK.py
@@ -2,7 +2,7 @@
 
 #%% Setup connection to the WFA SDK
 import windfarmer.sdk
-windfarmer_installation_folder = r'C:\Program Files\DNV\WindFarmer - Analyst 1.3.6.2'
+windfarmer_installation_folder = r'C:\Program Files\DNV\WindFarmer - Analyst 1.6.5.1'
 wf = windfarmer.sdk.Sdk(windfarmer_installation_folder)
 print(' > SDK is now up and running!')
 
